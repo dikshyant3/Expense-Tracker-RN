0a40976aac211e7a038d025194ffa6e0b5f52b3a
dikshyant3 <dhunganadikshyant12@gmail.com>
2025-06-10 16:34:46 +0545

    connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
 
 const app = express()
-app.listen(5001, ()=>{
-    console.log("Server is running on port 5001")
+dotenv.config()
+
+const PORT = process.env.PORT || 5001
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
 })
