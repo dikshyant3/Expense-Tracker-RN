7fceab36e7a2c3a5fafac98ca3924e535018f078
dikshyant3 <dhunganadikshyant12@gmail.com>
2025-06-14 15:45:03 +0545

    setting up clerk authentication

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,5 +1,15 @@
-import { Stack } from "expo-router";
+import { Stack, Slot } from "expo-router";
+import SafeScreen from "@/components/SafeScreen";
+
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { ClerkProvider } from '@clerk/clerk-expo'
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
+  );
 }
